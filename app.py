b33cd1a5488b102bd0b7e25222b04ca3ba1e94ab
DHRUV KUMAR <122805988+DHRUVVKUMAR@users.noreply.github.com>
2025-04-11 04:40:14 +0530

    fixing bugs

--- app.py
+++ app.py
@@ -97,6 +97,5 @@ def predict():
 
 # Start the server
 if __name__ == '__main__':
-    if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # default to 5000 locally
     app.run(host='0.0.0.0', port=port)
